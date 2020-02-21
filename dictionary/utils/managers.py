8702202b2d354a8edc09fc4d67dbfc5c350707cd
suayip <suayip.541@gmail.com>
2020-02-22 02:48:11 +0300

    split TopicListManager into TopicQueryHandler and TopicListHandler

--- dictionary/utils/managers.py
+++ dictionary/utils/managers.py
@@ -12,95 +12,46 @@ from django.utils import timezone
 from ..models import Category, Entry, Topic
 from ..models.managers.topic import TopicManager
 from ..utils import parse_date_or_none
-from ..utils.settings import (LOGIN_REQUIRED_CATEGORIES, NON_DB_CATEGORIES, TIME_THRESHOLD_24H, TOPICS_PER_PAGE_DEFAULT,
-                              UNCACHED_CATEGORIES)
+from ..utils.settings import (LOGIN_REQUIRED_CATEGORIES, NON_DB_CATEGORIES, TIME_THRESHOLD_24H,
+                              TOPICS_PER_PAGE_DEFAULT, UNCACHED_CATEGORIES)
 
 
-class TopicListManager:
+class TopicQueryHandler:
     """
-    Topic List Manager. views => left_frame for desktop and /basliklar/ for mobile.
-    Each non-database category has its own function, note that function names correspond to their slugs, this allows
+    Queryset algorithms for topic lists.
+    Each non-database category has its own method, note that function names correspond to their slugs, this allows
     us to write a clean code, and each time we have to edit one of them for specific feature it won't be painful.
     """
-    # constants
-    cache_timeout = 90  # 1.5 minutes, some categories have their exclusively set
 
-    # manager related stuff
-    data = None
-    slug_identifier = "/topic/"
-    cache_exists = False
-    cache_key = None
-
-    # queryset filters
+    # Queryset filters
     day_filter = {"entries__date_created__gte": TIME_THRESHOLD_24H}
     base_filter = {"entries__is_draft": False, "entries__author__is_novice": False, "is_censored": False}
 
-    # queryset annotations
+    # Queryset annotations
     base_annotation = {"latest": Max('entries__date_created')}  # to order_by("-latest")
     base_count = {"count": Count("entries", filter=Q(**day_filter))}
 
-    # queryset values
+    # Queryset values
     values = ["title", "slug", "count"]
     values_entry = values[:2]  # values with count excluded (used for entry listing)
 
-    def __init__(self, user=None, slug=None, year=None, fetch_cached=True, search_keys=None):
-        """
-        :param user: only pass request.user, required for topics per page and checking categories with login requirement
-        :param slug: slug of the category
-        :param year: only required for tarihte-bugun
-        :param fetch_cached: set to False if you don't want to fetch cached (refresh button behaviour).
-        :param search_keys request.GET for "hayvan-ara" (advanced search).
-        """
+    def bugun(self, user):
+        blocked = user.blocked.all()  # To exclude blocked users' topics.
+        return Topic.objects.filter(Q(category__in=user.following_categories.all()) | Q(category=None),
+                                    **self.base_filter, **self.day_filter, ).order_by('-latest').annotate(
+            **self.base_annotation, **self.base_count).exclude(created_by__in=blocked).values(*self.values)
 
-        self.user = user if user is not None else AnonymousUser
-
-        if not self.user.is_authenticated and slug in LOGIN_REQUIRED_CATEGORIES:
-            raise PermissionDenied("User not logged in")
-
-        if self.user.is_authenticated:
-            blocked = self.user.blocked.all()
-            self.auth_exclude_filter = {"created_by__in": blocked}  # use ONLY with LOGIN_REQUIRED_CATEGORIES
-
-        if slug in ["takip", "debe"]:
-            self.slug_identifier = "/entry/"
-        elif slug == "kenar":
-            self.slug_identifier = "/entry/update/"
-
-        self.slug = slug
-        self.year = year if self.slug == "tarihte-bugun" else None
-        self.search_keys = search_keys if self.slug == "hayvan-ara" else {}
-
-        if self.slug not in UNCACHED_CATEGORIES:
-            self.check_cache()
-
-            # user requests new data, so delete the old cached data
-            if not fetch_cached and self.cache_exists:
-                self.delete_cache()
-
-        if not self.cache_exists:
-            if slug in NON_DB_CATEGORIES:
-                # convert tarihte-bugun => tarihte_bugun, hayvan-ara => hayvan_ara (for getattr convenience)
-                slug = slug.replace("-", "_")
-                getattr(self, slug)()
-            else:
-                self.generic_category()
-
-    def bugun(self):
-        self.data = Topic.objects.filter(Q(category__in=self.user.following_categories.all()) | Q(category=None),
-                                         **self.base_filter, **self.day_filter, ).order_by('-latest').annotate(
-            **self.base_annotation, **self.base_count).exclude(**self.auth_exclude_filter).values(*self.values)
-
-    def tarihte_bugun(self):
+    def tarihte_bugun(self, year):
         now = timezone.now()
-        date_filter = {"entries__date_created__year": self.year, "entries__date_created__day": now.day,
+        date_filter = {"entries__date_created__year": year, "entries__date_created__day": now.day,
                        "entries__date_created__month": now.month}
 
-        self.data = Topic.objects.filter(**self.base_filter, **date_filter).order_by('-latest').annotate(
+        return Topic.objects.filter(**self.base_filter, **date_filter).order_by('-latest').annotate(
             **self.base_annotation, count=Count("entries", filter=Q(**date_filter))).order_by("-count").values(
             *self.values)
 
     def gundem(self):
-        self.data = [{'title': 'unimplemented yet', 'slug': 'unimplemented', 'count': 1}]
+        return [{'title': f'{self.__doc__}', 'slug': 'unimplemented', 'count': 1}]
 
     def debe(self):
         year, month, day = TIME_THRESHOLD_24H.year, TIME_THRESHOLD_24H.month, TIME_THRESHOLD_24H.day
@@ -108,34 +59,31 @@ class TopicListManager:
                                          topic__is_censored=False).order_by("-vote_rate").annotate(
             title=F("topic__title"), slug=F("pk")).values(*self.values_entry)
 
-        self.data = debe_list[:TOPICS_PER_PAGE_DEFAULT]
+        return debe_list[:TOPICS_PER_PAGE_DEFAULT]
 
-    def kenar(self):
-        if self.user.is_authenticated:
-            self.data = Entry.objects_all.filter(author=self.user, is_draft=True).order_by("-date_created").annotate(
-                title=F('topic__title'), slug=F("pk")).values(*self.values_entry)
+    def kenar(self, user):
+        return Entry.objects_all.filter(author=user, is_draft=True).order_by("-date_created").annotate(
+            title=F('topic__title'), slug=F("pk")).values(*self.values_entry)
 
-    def takip(self):
-        if self.user.is_authenticated:
-            self.data = Entry.objects.filter(date_created__gte=TIME_THRESHOLD_24H,
-                                             author__in=self.user.following.all()).order_by("-date_created").annotate(
-                title=Concat(F('topic__title'), Value("/#"), F('author__username')), slug=F("pk")).values(
-                *self.values_entry)
+    def takip(self, user):
+        return Entry.objects.filter(date_created__gte=TIME_THRESHOLD_24H, author__in=user.following.all()).order_by(
+            "-date_created").annotate(title=Concat(F('topic__title'), Value("/#"), F('author__username')),
+                                      slug=F("pk")).values(*self.values_entry)
 
     def caylaklar(self):
         caylak_filter = {"entries__author__is_novice": True, "entries__is_draft": False, "is_censored": False}
-        self.data = Topic.objects.filter(**self.day_filter, **caylak_filter).order_by('-latest').annotate(
+        return Topic.objects.filter(**self.day_filter, **caylak_filter).order_by('-latest').annotate(
             **self.base_annotation, **self.base_count).values(*self.values)
 
-    def hayvan_ara(self):
+    def hayvan_ara(self, user, search_keys):
         qs = Topic.objects
-        keywords = self.search_keys.get("keywords")
-        author_nick = self.search_keys.get("author_nick")
-        favorites_only = self.search_keys.get("is_in_favorites") == "true"
-        nice_only = self.search_keys.get("is_nice_ones") == "true"
-        from_date = self.search_keys.get("from_date")
-        to_date = self.search_keys.get("to_date")
-        orderding = self.search_keys.get("ordering")
+        keywords = search_keys.get("keywords")
+        author_nick = search_keys.get("author_nick")
+        favorites_only = search_keys.get("is_in_favorites") == "true"
+        nice_only = search_keys.get("is_nice_ones") == "true"
+        from_date = search_keys.get("from_date")
+        to_date = search_keys.get("to_date")
+        orderding = search_keys.get("ordering")
 
         count_filter = {"count": Count("entries", distinct=True)}
 
@@ -151,8 +99,8 @@ class TopicListManager:
             keywords = "akıl fikir"
 
         # Filtering
-        if favorites_only and self.user.is_authenticated:
-            qs = qs.filter(entries__favorited_by=self.user)
+        if favorites_only and user.is_authenticated:
+            qs = qs.filter(entries__favorited_by=user)
 
         if nice_only:
             qs = qs.annotate(nice_sum=Sum("entries__vote_rate")).filter(nice_sum__gte=Decimal("500"))
@@ -180,18 +128,76 @@ class TopicListManager:
                 qs = qs.order_by("-count", "-date_created")
 
             result = qs.values(*self.values)
-            self.data = result[:TOPICS_PER_PAGE_DEFAULT]
-        else:
-            self.data = qs.none()  # nothing found
+            return result[:TOPICS_PER_PAGE_DEFAULT]
 
-    def generic_category(self):
-        category = get_object_or_404(Category, slug=self.slug)
-        self.data = Topic.objects.filter(**self.base_filter, **self.day_filter, category=category).order_by(
+        return qs.none()  # nothing found
+
+    def generic_category(self, slug):
+        category = get_object_or_404(Category, slug=slug)
+        return Topic.objects.filter(**self.base_filter, **self.day_filter, category=category).order_by(
             '-latest').annotate(**self.base_annotation, **self.base_count).values(*self.values)
 
-    def basiboslar(self):  # No category supplied.
-        self.data = Topic.objects.filter(**self.base_filter, **self.day_filter, category=None).order_by(
-            '-latest').annotate(**self.base_annotation, **self.base_count).values(*self.values)
+    def basiboslar(self):
+        return Topic.objects.filter(**self.base_filter, **self.day_filter, category=None).order_by('-latest').annotate(
+            **self.base_annotation, **self.base_count).values(*self.values)
+
+
+class TopicListHandler:
+    """
+    Handles given topic slug and finds corresponding method in TopicQueryHandler to serialize data. Caches topic lists.
+    Handles authentication, returns general metadata in serialized data such as slug identifier & refresh count.
+    """
+
+    cache_timeout = 90  # 1.5 minutes, some categories have their exclusively set
+    data = None
+    slug_identifier = "/topic/"
+    cache_exists = False
+    cache_key = None
+
+    def __init__(self, user=None, slug=None, year=None, fetch_cached=True, search_keys=None):
+        """
+        :param user: only pass request.user, required for topics per page and checking categories with login requirement
+        :param slug: slug of the category
+        :param year: only required for tarihte-bugun
+        :param fetch_cached: set to False if you don't want to fetch cached (refresh button behaviour).
+        :param search_keys request.GET for "hayvan-ara" (advanced search).
+        """
+
+        self.user = user if user is not None else AnonymousUser
+
+        if not self.user.is_authenticated and slug in LOGIN_REQUIRED_CATEGORIES:
+            raise PermissionDenied("User not logged in")
+
+        self.slug = slug
+        self.year = year if self.slug == "tarihte-bugun" else None
+        self.search_keys = search_keys if self.slug == "hayvan-ara" else {}
+
+        if slug in ("takip", "debe"):
+            self.slug_identifier = "/entry/"
+        elif slug == "kenar":
+            self.slug_identifier = "/entry/update/"
+
+        if self.slug not in UNCACHED_CATEGORIES:
+            self.check_cache()
+
+            # user requests new data, so delete the old cached data
+            if not fetch_cached and self.cache_exists:
+                self.delete_cache()
+
+        if not self.cache_exists:
+            # Arguments to be passed for TopicQueryHandler methods. @formatter:off
+            arg_map = {
+                **dict.fromkeys(("bugun", "kenar", "takip"), [self.user]),
+                "tarihte_bugun": [self.year],
+                "generic_category": [self.slug],
+                "hayvan_ara": [self.user, self.search_keys]
+            }  # @formatter:on
+
+            # Convert tarihte-bugun => tarihte_bugun, hayvan-ara => hayvan_ara (for getattr convenience)
+            slug_method = self.slug.replace("-", "_") if self.slug in NON_DB_CATEGORIES else "generic_category"
+
+            # Get method from TopicQueryHandler.
+            self.data = getattr(self, slug_method)(*arg_map.get(slug_method, []))
 
     def cache_data(self, data):
         if self.slug in UNCACHED_CATEGORIES:
@@ -263,3 +269,11 @@ class TopicListManager:
             return Entry.objects.filter(date_created__gte=set_at).count()
 
         return 0
+
+
+class TopicListManager(TopicListHandler, TopicQueryHandler):
+    """
+    Base class for Topic List Management. Whenever a topic list is called, this manager is responsible for it.
+    To customize this, do not override this class, instead override each parent class seperately, and create a new
+    manager class. This way base classes will always be available.
+    """
