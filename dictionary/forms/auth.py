d0ccabbbb7d63be1cc0684258e36cd9b54ae1630
suayip <suayip.541@gmail.com>
2020-07-25 04:31:09 +0300

    i18n 4 (dictionary.forms)

--- dictionary/forms/auth.py
+++ dictionary/forms/auth.py
@@ -1,66 +1,79 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.forms.widgets import SelectDateWidget
+from django.utils.translation import gettext as _, gettext_lazy as _lazy
 
 from ..models import Author, AccountTerminationQueue
 
 
 class LoginForm(AuthenticationForm):
-    error_messages = {'invalid_login': (
-        "giriş yapılamadı. doğru e-posta ve parola kombinasyonunu girdiğinizden emin olun. bilgileriniz büyük-küçük "
-        "harf duyarlıdır. e-posta adresinizi onayladığınızdan emin olun.")}
-    remember_me = forms.BooleanField(required=False, label="beni hatırla")
+    error_messages = {
+        "invalid_login": (
+            _lazy(
+                "could not log in. make sure that you entered correct"
+                " combination of e-mail and password. your credentials are"
+                " case sensitive. make sure that you confirmed your e-mail."
+            )
+        )
+    }
+    remember_me = forms.BooleanField(required=False, label=_lazy("remember me"))
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254,
-                             help_text='Gerekli. Kayıt işlemini tamamlamak için geçerli bir mail adresi girin.',
-                             label="e-mail adresi")
-    gender = forms.ChoiceField(choices=Author.GENDERS, label="cinsiyet")
-    birth_date = forms.DateField(help_text='Gerekli.', widget=SelectDateWidget(years=range(2000, 1900, -1)),
-                                 label="doğum günü")
+    email = forms.EmailField(
+        max_length=254,
+        help_text=_lazy("required. in order to complete registration, provide a valid e-mail address."),
+        label=_lazy("e-mail"),
+    )
+    gender = forms.ChoiceField(choices=Author.GENDERS, label=_lazy("gender"))
+    birth_date = forms.DateField(widget=SelectDateWidget(years=range(2000, 1900, -1)), label=_lazy("birth date"))
     terms_conditions = forms.BooleanField(required=True)
 
     class Meta:
         model = Author
-        fields = ('username', 'email', 'password1', 'password2',)
-        labels = {'username': "takma isim"}
+        fields = (
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+        labels = {"username": _lazy("nickname")}
 
 
 class ResendEmailForm(forms.Form):
-    email = forms.EmailField(max_length=254, label="kayıt olurken kullandığınız e-posta adresiniz")
+    email = forms.EmailField(max_length=254, label=_lazy("the e-mail address you used to register your account"))
 
     def clean(self):
         if not self.errors:
             try:
-                author = Author.objects.get(email=self.cleaned_data.get('email'))
+                author = Author.objects.get(email=self.cleaned_data.get("email"))
                 if author.is_active:
-                    raise forms.ValidationError("bu e-posta çoktan onaylanmış")
+                    raise forms.ValidationError(_("this e-mail has already been confirmed."))
             except Author.DoesNotExist:
-                raise forms.ValidationError("böyle bir e-posta yok yalnız, hiç duymadım.")
+                raise forms.ValidationError(_("no such e-mail, never heard of it."))
 
         super().clean()
 
 
 class ChangeEmailForm(forms.Form):
-    email1 = forms.EmailField(max_length=254, label="yeni e-posta adresi")
-    email2 = forms.EmailField(max_length=254, label="yeni e-posta adresi (tekrar)")
-    password_confirm = forms.CharField(label="parolanızı teyit edin", strip=False, widget=forms.PasswordInput)
+    email1 = forms.EmailField(max_length=254, label=_lazy("new e-mail address"))
+    email2 = forms.EmailField(max_length=254, label=_lazy("new e-mail address (again)"))
+    password_confirm = forms.CharField(label=_lazy("confirm your password"), strip=False, widget=forms.PasswordInput)
 
     def clean(self):
         form_data = self.cleaned_data
 
         if form_data.get("email1") != form_data.get("email2"):
-            raise forms.ValidationError("e-postalar uyuşmadı")
+            raise forms.ValidationError(_("e-mails didn't match."))
 
         if Author.objects.filter(email=form_data.get("email1")).exists():
-            raise forms.ValidationError("bu e-posta kullanımda")
+            raise forms.ValidationError(_("this e-mail is already in use."))
 
         super().clean()
 
 
 class TerminateAccountForm(forms.ModelForm):
-    password_confirm = forms.CharField(label="parolanızı teyit edin", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label=_lazy("confirm your password"), strip=False, widget=forms.PasswordInput)
 
     class Meta:
         model = AccountTerminationQueue
--- dictionary/forms/edit.py
+++ dictionary/forms/edit.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
 from django.forms.widgets import SelectDateWidget
+from django.utils.translation import gettext as _, gettext_lazy as _lazy
 
 from ..models import Author, Entry, Memento, Message
 
@@ -8,12 +9,12 @@ from ..models import Author, Entry, Memento, Message
 class PreferencesForm(UserChangeForm):
     password = None
 
-    gender = forms.ChoiceField(choices=Author.GENDERS, label="cinsiyet")
-    birth_date = forms.DateField(widget=SelectDateWidget(years=range(2000, 1900, -1)), label="doğum günü")
-    entries_per_page = forms.ChoiceField(choices=Author.ENTRY_COUNTS, label="sayfa başına entry")
-    topics_per_page = forms.ChoiceField(choices=Author.TOPIC_COUNTS, label="sayfa başına başlık")
-    message_preference = forms.ChoiceField(choices=Author.MESSAGE_PREFERENCE, label="mesaj")
-    allow_receipts = forms.BooleanField(required=False, label="okundu bilgileri gösterilsin")
+    gender = forms.ChoiceField(choices=Author.GENDERS, label=_lazy("gender"))
+    birth_date = forms.DateField(widget=SelectDateWidget(years=range(2000, 1900, -1)), label=_lazy("birth date"))
+    entries_per_page = forms.ChoiceField(choices=Author.ENTRY_COUNTS, label=_lazy("entries per page"))
+    topics_per_page = forms.ChoiceField(choices=Author.TOPIC_COUNTS, label=_lazy("topics per page"))
+    message_preference = forms.ChoiceField(choices=Author.MESSAGE_PREFERENCE, label=_lazy("message"))
+    allow_receipts = forms.BooleanField(required=False, label=_lazy("show read receipts"))
 
     class Meta:
         model = Author
@@ -25,7 +26,7 @@ class EntryForm(forms.ModelForm):
         model = Entry
         fields = ("content", "is_draft")
         error_messages = {
-            "content": {"required": "bebeğim, alt tarafı bir entry gireceksin, ne kadar zor olabilir ki?"}
+            "content": {"required": _lazy("my dear, just write your entry, how hard could it be?")}
         }
 
 
@@ -34,22 +35,22 @@ class SendMessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ("body",)
-        labels = {"body": "mesajınız"}
+        labels = {"body": _lazy("message")}
 
-        error_messages = {"body": {"required": "ne diyorsun anlaşılmıyor"}}
+        error_messages = {"body": {"required": _lazy("can't really understand you")}}
 
     def clean(self):
         msg = self.cleaned_data.get("body", "")
 
         if len(msg) < 3:
-            raise forms.ValidationError("bu mesaj çok kısa be koçum")
+            raise forms.ValidationError(_("that message is just too short"))
 
         super().clean()
 
 
 class StandaloneMessageForm(SendMessageForm):
     # Used to create new conversations (in messages list view)
-    recipient = forms.CharField(label="kime")
+    recipient = forms.CharField(label=_lazy("to who"))
 
 
 class MementoForm(forms.ModelForm):
