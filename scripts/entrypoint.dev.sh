c8105fe2f9ae6e03f005cd6d95475b0cba695fb2
şuayip üzülmez <suayip.541@gmail.com>
2022-11-30 21:07:18 +0300

    Remove unnecessary command from dev entrypoint

--- scripts/entrypoint.dev.sh
+++ scripts/entrypoint.dev.sh
@@ -11,8 +11,6 @@ if [ "$DATABASE" = "postgres" ]; then
 fi
 
 export DJANGO_SUPERUSER_PASSWORD=test
-python manage.py makemigrations
-python manage.py migrate
 python manage.py create_generic_user superuser BVT8WnWNF8wJvb4K superuser@example.com --no-input
 python manage.py create_generic_user private BVT8WnWNF8wJvb4K private@example.com --no-input
 python manage.py createsuperuser --username admin --email test@django.org --is_active 1 --no-input
