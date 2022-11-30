763c288625cc1212276f1853f976bedfc8e1d4e2
şuayip üzülmez <suayip.541@gmail.com>
2022-11-30 22:26:56 +0300

    Work on production setup

--- djdict/settings_prod.py
+++ djdict/settings_prod.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
 
 GRAPHENE = {"SCHEMA": "dictionary_graph.schema.schema"}
