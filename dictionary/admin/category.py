133beaecf9d8c328d2a57f0dd0b957c4d9edf67c
şuayip üzülmez <suayip.541@gmail.com>
2022-12-10 13:53:28 +0300

    Minor improvement in category admin

--- dictionary/admin/category.py
+++ dictionary/admin/category.py
@@ -7,7 +7,7 @@ from dictionary.models import Category, Suggestion
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ("name",)
-    list_editable = ("weight",)
+    list_editable = ("weight", "is_pseudo", "is_default")
     list_display = ("name", "weight", "description", "is_default", "is_pseudo")
     exclude = ("slug",)
 
